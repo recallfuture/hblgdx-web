dd03ad4ccc765412c5716e459f6e4f04c7424841
su <recallsufuture@gmail.com>
2019-08-28 12:37:03 +0800

    完善作业查询页面

--- pages/homework.vue
+++ pages/homework.vue
@@ -1,9 +1,27 @@
 <template>
-  <div></div>
+  <div>
+    <v-card class="mb-2">
+      <v-card-text class="text-center">
+        有0门课程有待交作业
+      </v-card-text>
+    </v-card>
+
+    <v-card v-for="(course, index) in courses" :key="index" class="mb-2">
+      <v-card-text class="d-flex flex-row align-center">
+        <v-icon color="orange" class="mr-4">bookmark</v-icon>
+        {{ course.name }}
+      </v-card-text>
+    </v-card>
+  </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      courses: [{ id: '1', name: 'c++' }, { id: '1', name: 'c#' }]
+    }
+  },
   mounted() {
     this.$store.commit('changeTitle', '作业查询')
   }
