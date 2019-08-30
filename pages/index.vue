17b056d3c1ee48626333ee138d98a430d404e90b
su <recallsufuture@gmail.com>
2019-08-30 20:39:58 +0800

    修改主页

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,13 @@
 <template>
-  <v-layout column justify-center align-center> </v-layout>
+  <v-container>
+    <h1 class="text-center">欢迎使用校园查！</h1>
+  </v-container>
 </template>
 
 <script>
-export default {}
+export default {
+  mounted() {
+    this.$store.commit('updateTitle', '首页')
+  }
+}
 </script>
